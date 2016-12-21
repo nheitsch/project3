b849491636686b49662f6b400b1f709fba6db2ab
novaskyland <nick.heitsch@gmail.com>
2016-12-21 17:43:25 -0500

    js for the website
    validator contains the validation from lab10 for the contacts form.
    menu-highlighter from lab06 is for the navigation menu to reflect which
    page the user is on

---
+++ js/validator.js
@@ -0,0 +1,27 @@
+
+   <!--
+      // Form validation code will come here.
+      function validate()
+      {
+      
+         if( document.myForm.Name.value == "" )
+         {
+            alert( "Please provide your name!" );
+            document.myForm.Name.focus() ;
+            return false;
+         }
+
+         //EMail validator
+         
+         var emailID = document.myForm.EMail.value;
+         atpos = emailID.indexOf("@");
+         dotpos = emailID.lastIndexOf(".");
+         
+         if (atpos < 1 || ( dotpos - atpos < 2 )) 
+         {
+            alert("Please enter correct email ID")
+            document.myForm.EMail.focus() ;
+            return false;
+         }
+         //end Email
+      }
